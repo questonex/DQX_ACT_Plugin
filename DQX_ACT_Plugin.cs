821880d3f0ea58cef424fda634fd46963de65c49
questonex <questonex@gmail.com>
2016-05-16 18:47:54 +0900

    fix action regexp

--- DQX_ACT_Plugin.cs
+++ DQX_ACT_Plugin.cs
@@ -634,7 +634,7 @@ namespace DQX_ACT_Plugin
 
     public static Regex regex_open = new Regex(@"(?<target>[^ ]+?)と 戦闘開始！$");
     public static Regex regex_close = new Regex(@" (やっつけた！|いなくなった！|ぜんめつした。|戦いをやめた。)$");
-    public static Regex regex_action = new Regex(@"^(?<actor>.+?)(の|は) (?<action>.+?)！$");
+    public static Regex regex_action = new Regex(@"^(?<actor>.+?)(の|は|が) (?<action>.+?)(を まいおどった|をうたった|)！$");
     public static Regex regex_action2 = new Regex(@"^(?<action>[^ ]+?)！$");
     public static Regex regex_hit = new Regex(@"^ → (?<target>.+?)(に|は) (?<damage>\d+)のダメージ(！|を うけた！)$");
     public static Regex regex_miss = new Regex(@"^ → ミス！ (?<target>.+?)(に|は) ダメージを (あたえられない|うけない)！$");
